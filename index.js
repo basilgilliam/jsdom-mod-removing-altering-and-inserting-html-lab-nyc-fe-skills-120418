250ff07d587514a7bd89ab9d9fcae29f7c232e2c
Basil Gilliam <basilgilliam@gmail.com>
2019-01-15 23:04:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 /* Enter the code to remove the main node element under this comment */
+const main= document.querySelector('main')
+main.remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+const newHeader = document.createElement('h1')
+h1.innerText('YERRRRR')
+document.body.appendChild(newHeader)
+
+
+
+
+
